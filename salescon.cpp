51e29e0f831e4b389eacdd0a3db5945dafc4cce1
Simon Schmoll <simon.schmoll@gmx.de>
2019-01-22 18:16:38 +0100

    advanced contract with set item functionality coupled with authorisations

--- salescon.cpp
+++ salescon.cpp
@@ -6,16 +6,28 @@ class [[eosio::contract]] salescon : public eosio::contract {
   public:
     using contract::contract;
 
-  salescon(name receiver, name code,  datastream<const char*> ds): contract(receiver, code, ds) {}
+  salescon(name receiver, name code,  datastream<const char*> ds): contract(receiver, code, ds), _item(_code, _code.value),
+    _config(_code, _code.value), _agree(_code, _code.value) {}
 
   [[eosio::action]]
-  void setitem(name user, std::string itemName, uint64_t itemPrice,
+  void init(name seller, name buyer, name intermediator) {
+    auto iterator = _config.find(0);
+    eosio_assert(iterator == _config.end(), "Contract already initialized");
+    _config.emplace(seller, [&] ( auto& row ) {
+      row.seller = seller;
+      row.buyer = buyer; 
+      row.intermediator = intermediator;
+    });
+  }
+
+  [[eosio::action]]
+  void setitem(std::string itemName, uint64_t itemPrice,
     bool itemReceived, bool itemPaid) {
-    print("Hi");
-    itemstr item(_code, _code.value);
-    auto iterator = item.find(0);
-    eosio_assert(iterator == item.end(), "Item already set");
-    item.emplace(user, [&]( auto& row ) {
+    require_auth(getSeller());
+    assertInitialized();
+    auto iterator = _item.find(0);
+    eosio_assert(iterator == _item.end(), "Item already set");
+    _item.emplace(getSeller(), [&]( auto& row ) {
      row.key = 0;
      row.itemName = itemName;
      row.itemPrice = itemPrice;
@@ -23,6 +35,7 @@ class [[eosio::contract]] salescon : public eosio::contract {
      row.itemPaid = itemPaid;
     });
   }
+
 
   struct [[eosio::table]] agreestruct {
     uint64_t key;
@@ -54,5 +67,23 @@ class [[eosio::contract]] salescon : public eosio::contract {
   typedef eosio::multi_index<"config"_n, configstruct> configstr;
   typedef eosio::multi_index<"agreement"_n, agreestruct> agreementstr;
   typedef eosio::multi_index<"item"_n, itemstruct> itemstr;
+
+  configstr _config;
+  agreementstr _agree;
+  itemstr _item;
+private: 
+  void assertInitialized() {
+    // configstr _config(_code, _code.value);
+    auto iterator = _config.find(0);
+    eosio_assert(iterator != _config.end(), "Contract must be initialized!");
+  }
+
+  name getSeller() {
+    // configstr config(_code, _code.value);
+    auto iterator = _config.find(0);
+    eosio_assert(iterator != _config.end(), "Contract not initialized");
+    return (*iterator).seller;
+  }
+
 };
-EOSIO_DISPATCH(salescon, (setitem))
+EOSIO_DISPATCH(salescon, (setitem)(init))

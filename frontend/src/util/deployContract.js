c716b81a3784e2853efbdc8b4915470c0275079a
Simon Schmoll <simon.schmoll@googlemail.com>
2019-02-17 15:51:29 +0100

    removed commented lines

--- frontend/src/util/deployContract.js
+++ frontend/src/util/deployContract.js
@@ -1,32 +1,15 @@
 import eos from './eos';
 
 const { TextDecoder, TextEncoder } = require('text-encoding');
-
-// const wasm = require('./salescon.wasm');
-// const fs = require('fs');
-// const path = require('path');
 const { Serialize } = require('eos-transit/node_modules/eosjs');
-// function getDeployableFilesFromDir(dir) {
-//   const dirCont = fs.readdirSync(dir);
-//   const wasmFileName = dirCont.find(filePath => filePath.match(/.*\.(wasm)$/gi));
-//   const abiFileName = dirCont.find(filePath => filePath.match(/.*\.(abi)$/gi));
-//   if (!wasmFileName) throw new Error(`Cannot find a ".wasm file" in ${dir}`);
-//   if (!abiFileName) throw new Error(`Cannot find an ".abi file" in ${dir}`);
-//   return {
-//     wasmPath: path.join(dir, wasmFileName),
-//     abiPath: path.join(dir, abiFileName),
-//   };
-// }
+
 
 async function deployContract() {
   const wallet = await eos.getSellerWallet();
   console.log('eos', wallet);
 
   const api = wallet.eosApi;
-  // const { wasmPath, abiPath } = getDeployableFilesFromDir(contractDir);
 
-  // 1. Prepare SETCODE
-  // read the file and make a hex string out of it
   const wasmBuffer = await fetch('./salescon.wasm')
     .then((res) => {
       if (res.ok) {
@@ -40,13 +23,6 @@ async function deployContract() {
     });
 
   console.log('wasm buffer', wasmBuffer);
-  // const wasm = fs.readFileSync('./salescon.wasm').toString('hex');
-
-  // // 2. Prepare SETABI
-  // const buffer = new Serialize.SerialBuffer({
-  //   textEncoder: new TextEncoder(),
-  //   textDecoder: new TextDecoder(),
-  // });
 
   const abiBuffer = await fetch('./salescon.abi')
     .then((res) => {
@@ -56,15 +32,14 @@ async function deployContract() {
       throw new Error('Unable to fetch abi.');
     })
     .then((bytes) => {
-      console.log('Bytes abi', bytes);
-      return JSON.parse(Buffer.from(bytes).asUint8Array());
+      console.log('Bytes abi', Buffer.from(bytes));
+      return Buffer.from(bytes);
     });
 
-  let abiJson = await api.rawAbiToJson(abiBuffer);
+  // let abiJson = await api.rawAbiToJson(abiBuffer); // can not convert abi to json
 
   const abiDefinition = api.abiTypes.get('abi_def');
-  // need to make sure abi has every field in abiDefinition.fields
-  // otherwise serialize throws
+
   abiJson = abiDefinition.fields.reduce(
     (acc, { name: fieldName }) => Object.assign(acc, { [fieldName]: acc[fieldName] || [] }),
     abiJson,
@@ -75,17 +50,7 @@ async function deployContract() {
     textDecoder: new TextDecoder(),
   });
   abiDefinition.serialize(buffer, abiJson);
-  // let abi = JSON.parse(fs.readFileSync('./salescon.abi', 'utf8'));
-  // // let abi = JSON.parse(fs.readFileSync(abiPath, 'utf8'));
-  // const abiDefinition = api.abiTypes.get('abi_def');
-  // // need to make sure abi has every field in abiDefinition.fields
-  // // otherwise serialize throws
-  // abi = abiDefinition.fields.reduce(
-  //   (acc, { name: fieldName }) => Object.assign(acc, { [fieldName]: acc[fieldName] || [] }),
-  //   abi,
-  // );
-  // abiDefinition.serialize(buffer, abi);
-  // // 3. Send transaction with both setcode and setabi actions
+
   return api.transact(
     {
       actions: [

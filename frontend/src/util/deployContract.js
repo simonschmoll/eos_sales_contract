cae4b856809105d8b4574ed28a64f59087fc1200
Simon Schmoll <simon.schmoll@googlemail.com>
2019-02-18 15:34:21 +0100

    modified deploy functionality

--- frontend/src/util/deployContract.js
+++ frontend/src/util/deployContract.js
@@ -1,8 +1,13 @@
+import abi from '!raw-loader!./salescon.abi';
+// import wasm from '!raw-loader!./salescon.txt';
 import eos from './eos';
 
 const { TextDecoder, TextEncoder } = require('text-encoding');
 const { Serialize } = require('eos-transit/node_modules/eosjs');
 
+function buf2hex(buffer) { // buffer is an ArrayBuffer
+  return Array.prototype.map.call(new Uint8Array(buffer), x => (`00${x.toString(16)}`).slice(-2)).join('');
+}
 
 async function deployContract() {
   const wallet = await eos.getSellerWallet();
@@ -18,11 +23,14 @@ async function deployContract() {
       throw new Error('Unable to fetch WASM.');
     })
     .then((bytes) => {
-      console.log('Bytes', Buffer.from(bytes));
-      return Buffer.from(bytes);
+      console.log('Bytes', bytes);
+      return buf2hex(bytes);
     });
 
   console.log('wasm buffer', wasmBuffer);
+
+  // wasmBuffer = Buffer.from(wasmBuffer.data);
+  // console.log('Abi from import', abi);
 
   const abiBuffer = await fetch('./salescon.abi')
     .then((res) => {
@@ -36,21 +44,24 @@ async function deployContract() {
       return Buffer.from(bytes);
     });
 
-  // let abiJson = await api.rawAbiToJson(abiBuffer); // can not convert abi to json
-
+  // let abiJson = await api.rawAbiToJson(abi); // can not convert abi to json
+  // abiBuffer = abiBuffer.toString('utf8');
   const abiDefinition = api.abiTypes.get('abi_def');
+  let abiJson = JSON.parse(abi.toString('hex'));
+  console.log(abiJson);
 
   abiJson = abiDefinition.fields.reduce(
     (acc, { name: fieldName }) => Object.assign(acc, { [fieldName]: acc[fieldName] || [] }),
     abiJson,
   );
 
+  console.log('Abi json is', abiJson);
   const buffer = new Serialize.SerialBuffer({
     textEncoder: new TextEncoder(),
     textDecoder: new TextDecoder(),
   });
   abiDefinition.serialize(buffer, abiJson);
-
+  console.log('Abi after ser', Buffer.from(buffer.asUint8Array()).toString('hex'));
   return api.transact(
     {
       actions: [
@@ -64,7 +75,7 @@ async function deployContract() {
             },
           ],
           data: {
-            account: wallet.auth.accountName,
+            account: 'seller',
             vmtype: 0,
             vmversion: 0,
             code: wasmBuffer,
@@ -80,7 +91,7 @@ async function deployContract() {
             },
           ],
           data: {
-            account: wallet.auth.accountName,
+            account: 'seller',
             abi: Buffer.from(buffer.asUint8Array()).toString('hex'),
           },
         },

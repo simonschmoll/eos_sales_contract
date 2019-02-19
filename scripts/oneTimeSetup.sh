2264412d344b6b42169470acaa16d29144973c3a
Simon Schmoll <simon.schmoll@googlemail.com>
2019-02-19 10:55:58 +0100

    added import of other necessary keys

--- scripts/oneTimeSetup.sh
+++ scripts/oneTimeSetup.sh
@@ -13,3 +13,5 @@ nohup keosd &>/dev/null &
 cleos wallet create --file pw.txt
 # pw=$(sed -e 's/.*"\(.*\)".*/\1/' pw.txt)
 cleos wallet import --private-key 5K8ghcBf9TpPAWdxHDUejqcxBWrQAzkj5D5FWHe13nmNJmWhH9k
+cleos wallet import --private-key 5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3
+cleos wallet import --private-key 5KFcKixjbRQsfq9cQ8YXsV6GHEHqaiRrfGLevnM9AqUw5L1PmKC

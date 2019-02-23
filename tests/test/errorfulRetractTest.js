ce6b4d9a60ba7cf663eac5c9e7298e45fa7c0b0b
Simon Schmoll <simon.schmoll@googlemail.com>
2019-02-23 15:15:26 +0100

    additional test for retraction and better separation of tests

--- tests/test/errorfulRetractTest.js
+++ tests/test/errorfulRetractTest.js
@@ -16,6 +16,10 @@ describe('Errorful retracting contract tests', () => {
    retract() tests
   /**********************************************************************************/
     
+  /***********************************************************************************
+   retractContract test without permission
+  /**********************************************************************************/
+
   it('Person outside the contract wants to retract', async () => {
     try {
       // When
@@ -26,6 +30,62 @@ describe('Errorful retracting contract tests', () => {
       assert.deepEqual('Error: assertion failure with message: Caller does not have the permission to call this method', error.toString());
     }
   })
+
+/***********************************************************************************
+ retractContract test while contract not intact
+/**********************************************************************************/
+
+  it('Retraction after contract is not intact anymore', async () => {
+    try {
+      // When
+      await testService.retract('buyer');
+      await testService.retract('intermed');
+      await new Promise(resolve => setTimeout(resolve, 500)); // Necessary otherwise rpc will say that it is a duplicate transaction
+      await testService.retract('intermed');
+      assert.fail()
+    } catch (error) {
+      // Then
+      assert.deepEqual('Error: assertion failure with message: Contract is closed', error.toString());
+    }
+  })  
+
+  /***********************************************************************************
+   retractContract test, attempt by third participant to also retract
+  /**********************************************************************************/
+
+  it('Contract is retracted (intermediator, buyer), buyer tries to retract', async () => {
+    await testService.setItem('seller');
+    await testService.pay('buyer');
+    await testService.retract('buyer');
+    await testService.retract('intermed');
+    try {
+      // When
+      await testService.retract('seller');
+      assert.fail()
+    } catch (error) {
+      // Then
+      assert.deepEqual('Error: assertion failure with message: Contract must not be retracted', error.toString());
+    }
+  })
+
+  it('Contract is retracted (intermediator, seller), buyer tries to retract', async () => {
+    await testService.setItem('seller');
+    await testService.pay('buyer');
+    await testService.retract('seller');
+    await testService.retract('intermed');
+    try {
+      // When
+      await testService.retract('buyer');
+      assert.fail()
+    } catch (error) {
+      // Then
+      assert.deepEqual('Error: assertion failure with message: Contract must not be retracted', error.toString());
+    }
+  })
+
+  /***********************************************************************************
+   retractContract test seller and buyer try both to retract while contract is intact
+  /**********************************************************************************/
 
   it('Buyer wants retract after seller', async () => {
     try {
@@ -48,35 +108,6 @@ describe('Errorful retracting contract tests', () => {
     } catch (error) {
       // Then
       assert.deepEqual('Error: assertion failure with message: Can not retract, because buyer already retracted', error.toString());
-    }
-  })
-
-  it('Retraction after contract is not intact anymore', async () => {
-    try {
-      // When
-      await testService.retract('buyer');
-      await testService.retract('intermed');
-      await new Promise(resolve => setTimeout(resolve, 500)); // Necessary otherwise rpc will say that it is a duplicate transaction
-      await testService.retract('intermed');
-      assert.fail()
-    } catch (error) {
-      // Then
-      assert.deepEqual('Error: assertion failure with message: Contract is closed', error.toString());
-    }
-  })
-
-  it('Retraction after contract is already retracted', async () => {
-    try {
-      // When
-      await testService.retract('intermed');
-      await testService.setItem('seller');
-      await testService.pay('buyer');
-      await testService.retract('buyer');
-      await testService.retract('intermed');
-      assert.fail()
-    } catch (error) {
-      // Then
-      assert.deepEqual('Error: assertion failure with message: Contract must not be retracted', error.toString());
     }
   })
 })

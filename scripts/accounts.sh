24e9ba2574ea52c82c4162825d947afd6b475215
Simon Schmoll <simon.schmoll@googlemail.com>
2019-04-04 10:51:25 +0200

    swwitch back to 3 accounts with same key

--- scripts/accounts.sh
+++ scripts/accounts.sh
@@ -3,7 +3,7 @@
 
 cat pw.txt | cleos wallet unlock
 
-cleos create account eosio seller EOS5EUcX6pBvNwgcvFjatgBR9uTvobpz9AxaZJrveD6kYzsJ6nZjS
-cleos create account eosio buyer EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV
-cleos create account eosio intermed EOS7YmdcJwNtKBVgAqxDtij36paHBzPSXXQU1Fyd2Sygmi7QnYGb2
+cleos create account eosio seller EOS8EXyGVLjxTPyaZyEqJDzthx1SPNec3g13MgcYbNQTLv9KC5wkL
+cleos create account eosio buyer EOS8EXyGVLjxTPyaZyEqJDzthx1SPNec3g13MgcYbNQTLv9KC5wkL
+cleos create account eosio intermed EOS8EXyGVLjxTPyaZyEqJDzthx1SPNec3g13MgcYbNQTLv9KC5wkL
 cleos create account eosio random EOS8EXyGVLjxTPyaZyEqJDzthx1SPNec3g13MgcYbNQTLv9KC5wkL
